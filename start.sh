5f11c6a4a51ee39100ce6611a405c93112940cc7
leviathan0992@gmail.com <leviathan0992@gmail.com>
2020-06-26 17:18:55 +0800

    rename spleen

--- start.sh
+++ start.sh
@@ -2,19 +2,19 @@
 
 if [ "$1" = "client" ]
 then
-  sudo mkdir -p /root/proxy/
+  sudo mkdir -p /root/spleen/
   sudo cp spleen-client.service /etc/systemd/system/
-  sudo cp spleen-client /root/proxy/
-  sudo cp .client.json /root/proxy/
+  sudo cp spleen-client /root/spleen/
+  sudo cp .client.json /root/spleen/
   systemctl enable spleen-client
   systemctl start spleen-client
   systemctl status spleen-client -l
 elif [ "$1" = "server" ]
 then
-  sudo mkdir -p /root/proxy/
+  sudo mkdir -p /root/spleen/
   sudo cp spleen-server.service /etc/systemd/system/
-  sudo cp spleen-server /root/proxy/
-  sudo cp .server.json /root/proxy/
+  sudo cp spleen-server /root/spleen/
+  sudo cp .server.json /root/spleen/
   systemctl enable spleen-server
   systemctl start spleen-server
   systemctl status spleen-server -l
