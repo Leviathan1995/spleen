97ae71920320242c5981871a2cb3f834640f9955
leviathan0992@gmail.com <leviathan0992@gmail.com>
2022-01-16 17:57:02 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,18 +6,18 @@ then
   sudo cp spleen-client.service /etc/systemd/system/
   sudo cp spleen-client /etc/spleen/
   sudo cp .client.json /etc/spleen/
-  systemctl enable spleen-client
-  systemctl start spleen-client
-  systemctl status spleen-client -l
+  sudo systemctl enable spleen-client
+  sudo systemctl start spleen-client
+  sudo systemctl status spleen-client -l
 elif [ "$1" = "server" ]
 then
   sudo mkdir -p /etc/spleen/
   sudo cp spleen-server.service /etc/systemd/system/
   sudo cp spleen-server /etc/spleen/
   sudo cp .server.json /etc/spleen/
-  systemctl enable spleen-server
-  systemctl start spleen-server
-  systemctl status spleen-server -l
+  sudo systemctl enable spleen-server
+  sudo systemctl start spleen-server
+  sudo systemctl status spleen-server -l
 else
   echo "Invalid parameter"
 fi
